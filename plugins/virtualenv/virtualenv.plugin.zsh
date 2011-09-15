87950f14774891c7faca8673bd3b31e580a0e970
Vladimir Pouzanov <farcaller@gmail.com>
2011-09-15 22:28:12 +0300

    default venv -- with site packages

--- plugins/virtualenv/virtualenv.plugin.zsh
+++ plugins/virtualenv/virtualenv.plugin.zsh
@@ -1,4 +1,4 @@
 export WORKON_HOME=$HOME/.pyenv
 export PROJECT_HOME=$HOME/Developer
-export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
+#export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 source /usr/local/bin/virtualenvwrapper.sh
